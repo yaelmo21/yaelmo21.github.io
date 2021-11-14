2281e93c60a1761218dda12029f11cbd9fdc872b
Yael Ruiz Pucheta <yaelruiz99@outlook.com>
2021-11-14 14:57:08 -0600

    PWA

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+
 AOS.init();
 
 const swiper = new Swiper(".mySwiper", {
---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+function updateCacheDynamic(dynamicCache, req, res) {
+    if (res.ok) {
+        return caches.open(dynamicCache)
+            .then(cache => {
+                cache.put(req, res.clone());
+                return res.clone();
+            });
+    } else {
+        return res;
+    }
+}
