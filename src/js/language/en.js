c7d08d6d745952fbf2460bd2630dc0dc5b050500
Yael Ruiz Pucheta <yaelruiz99@outlook.com>
2022-04-09 11:20:08 -0500

    Fix English file

--- src/js/language/en.js
+++ src/js/language/en.js
@@ -5,13 +5,13 @@ export const englishText = () => {
     elements.navAbout.innerText = "About";
     elements.navSkills.innerText = "Skills";
     elements.navCertifications.innerText = "Certifications";
-    elements.navLatestProyects.innerText = "Latest Proyects";
+    elements.navLatestProyects.innerText = "Last Projects";
     elements.navContact.innerText = "Contact";
 
     elements.aboutTitle.innerText = "About me";
     elements.description.innerText = "I grew up in the city of Xalapa, Veracruz, Mexico, I started in the world of software development in 2015, specializing in web development. Within the industry I have worked in sectors such as finance and health sciences, I have also served as a teacher / mentor sharing my knowledge and experiences in the world of technology. Teaching and programming beyond being my job, are my great passions.";
     elements.skillTitle.innerText = "Skills";
-    elements.lastesProyectsTitle.innerText = "Lastes projects";
+    elements.lastesProyectsTitle.innerText = "Last Projects";
     elements.certificationTitle.innerText = 'Certifications';
     elements.contacTitle.innerText = "Contact me";
     elements.teleimmDescription.innerText = 'Medical social network';
